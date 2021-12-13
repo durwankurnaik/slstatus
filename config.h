54dbd48f8b53415e3093751d2e69939d1c1a42af
Durwankur Naik <ndurwankur14@gmail.com>
2021-12-14 01:54:00 +0530

    Some minor changes
    Just updating the status bar a little bit

--- config.h
+++ config.h
@@ -67,8 +67,8 @@ static const struct arg args[] = {
 	/* function format          argument */
 	{ run_command, "[  %s", "/home/sparrxw/.local/bin/slstatus_files/battery" },
 	{ battery_perc,	"%s%% ] ", "BAT1" },
-	{ netspeed_rx, "[  %sB/s ] ", "wlp8s0" },
-	//{ netspeed_rx, "[  %sB/s ] ", "enp7s0" },
+	//{ netspeed_rx, "[  %sB/s ] ", "wlp8s0" },
+	{ netspeed_rx, "[  %sB/s ] ", "enp7s0" },
 	//{ netspeed_tx, " %sB/s ] ", "wlp8s0" },
 	{ run_command, "[  %s ] ", "awk -F\"[][]\" '/Left:/ { print $2 }' <(amixer sget Master)" },
 	{ ram_used, "[  %sB ] ", NULL },
